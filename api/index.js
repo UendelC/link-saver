36a08991888588efee175ced8f9a6b874d397642
Uendel Couto <uendel.couto@gmail.com>
2020-05-08 21:44:31 -0300

    Add api

---
+++ api/index.js
@@ -0,0 +1,41 @@
+const http = require('http');
+const URL = require('url');
+const fs = require('fs');
+const data = require('./urls.json');
+const path = require('path');
+
+function writeFile(callback) {
+    fs.writeFile(
+        path.join(__dirname, "urls.json"),
+        JSON.stringify(data, null, 2),
+        err => {
+            if (err) throw err;
+
+            callback(JSON.stringify({ message: "ok" }));
+        }
+    )
+}
+
+http.createServer((req, res) => {
+    const { name, url, del } = URL.parse(req.url, true).query;
+
+    res.writeHead(200, {
+        'Acess-Control-Allow-Origin': '*'
+    });
+
+    if (!name || !url) {
+        return res.end(JSON.stringify(data));
+    }
+
+    if (del) {
+        data.urls = data.urls.filter(item => String(item.url) !== String(url));
+        console.log(data.urls);
+        return writeFile(message => res.end(message));
+    }
+
+    data.urls.push({ name, url });
+
+    return writeFile(message => res.end(message));
+}).listen(3000, () => {
+    console.log('Api is running');
+})
